0226e3960aacf6b1edf7dc587725d99dc27bb93a
Carson Ray <carsonray314@gmail.com>
2025-08-05 18:33:12 -0400

    Added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -41,25 +41,25 @@ uint8_t busChainIDs[2] = {0, 1};
 #define I2C_BAUD_RATE 1000000
 
 namespace SerialHeaders {
-  	//Headers from master to controller
+	//Headers from master to controller
 
 	//Pings microcontroller
-	#define PING 0x1
+	#define PING 0x1 // 0 bytes
 	//Servo power update
-	#define SERVO_POWER 0x2
+	#define SERVO_POWER 0x2 // 3 bytes (1 byte motor id, 2 byte power value from 0 to 1)
 
 	//Headers from controller to master
 
 	//Acknowledges ping      
-	#define PING_ACK 0x1
+	#define PING_ACK 0x1 // 0 bytes
 	//PWM cycle start
-	#define PWM_CYCLE 0x2  
+	#define PWM_CYCLE 0x2 // 0 bytes
 	//Sends magnetic encoder data
-	#define MAGENCODER_DATA 0xA0
+	#define MAGENCODER_DATA 0xA0 // 5 bytes (1 byte sensor id, 4 byte Y and Z axes)
 	//Sends magnetic tracker data
-	#define MAGTRACKER_DATA 0xA1
+	#define MAGTRACKER_DATA 0xA1 // 7 bytes (1 byte sensor id, 6 byte X, Y and Z axes)
 	//Sends IMU data
-	#define IMU_DATA 0xA2                                                                                              
+	#define IMU_DATA 0xA2 // 13 bytes (1 byte sensor id, 6 byte 3-axis accel data, 6 byte 3-axis gyro data)                                                                                           
 }
 
 using namespace SerialHeaders;
