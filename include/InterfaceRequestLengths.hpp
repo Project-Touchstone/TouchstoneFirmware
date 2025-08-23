8635178662885a8317e9eefe7f691f212bdf9434
Carson Ray <carsonray314@gmail.com>
2025-08-23 01:55:07 -0400

    Finished configuration files

---
+++ include/InterfaceRequestLengths.hpp
@@ -0,0 +1,31 @@
+#ifndef INTERFACE_REQUEST_LENGTHS
+#define INTERFACE_REQUEST_LENGTHS
+
+#include <Arduino.h>
+#include <unordered_map>
+
+
+std::unordered_map<uint8_t, int16_t> lengthsByRequest = {
+    { 1,  0 },
+    { 16, 0 },
+    { 17, -1 },
+    { 18, 1 },
+    { 19, 2 },
+    { 20, 1 },
+    { 21, 2 },
+    { 22, 1 },
+    { 23, 2 },
+    { 24, 1 },
+    { 25, 2 },
+    { 26, 1 },
+    { 27, 2 },
+    { 32, 0 },
+    { 48, 3 },
+    { 49, 5 },
+    { 50, 5 },
+    { 51, 5 }
+};
+
+
+
+#endif
